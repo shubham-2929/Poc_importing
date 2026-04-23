7cce77f24644a76d1744af5472c4d1af3b04ea68
shubham-2929 <mahajanshubham029@gmail.com>
2026-04-23 14:37:04 +0000

    Delete projects/TestProject/com.inductiveautomation.webdev/resources/api/reload directory

--- projects/TestProject/com.inductiveautomation.webdev/resources/api/reload/doPost.py
+++
@@ -1,6 +0,0 @@
-def doPost(request, session):
-    try:
-        system.project.requestScan()
-        return {'json': {'status': 'ok', 'message': 'Scan triggered'}}
-    except Exception as e:
-        return {'json': {'status': 'error', 'message': str(e)}}
