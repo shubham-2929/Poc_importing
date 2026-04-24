b4c86812aaafe3a4fe3688b10b35c0ace0540176
shubham-2929 <mahajanshubham029@gmail.com>
2026-04-24 13:33:19 +0000

    Implement error handling in doGet function

--- projects/TestProject/com.inductiveautomation.webdev/resources/api/reloadTags/doGet.py
+++ projects/TestProject/com.inductiveautomation.webdev/resources/api/reloadTags/doGet.py
@@ -1,2 +1,14 @@
 def doGet(request, session):
+   try:
+        tags_file = "C:/Program Files/Inductive Automation/Ignition/data/config/resources/core/ignition/tag-definition/default/tags.json"
+
+        tags_json = system.file.readFileAsString(tags_file)
+
+        system.tag.configure("[default]", tags_json, 'o')
+
+        return {"json": {"status": "success"}}
+  except Exception as e:
+  		
+	    return {"json": {"status": "error", "message": str(e)}}
+
  
